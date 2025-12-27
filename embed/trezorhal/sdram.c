b5003f247e3dc3f3d60bdef43069f6489c232a1d
canon193 <canon193@gmail.com>
2025-12-27 10:58:43 +0000

    Update

--- embed/trezorhal/sdram.c
+++ embed/trezorhal/sdram.c
@@ -161,9 +161,8 @@ static void sdram_initialization_sequence(void) {
 
 void sdram_init(void) {
     // Check if SDRAM is already initialized (by bootloader)
-    // FMC clock enabled and SDRAM bank 2 not in power-down = already init
-    if (__HAL_RCC_FMC_IS_CLK_ENABLED() &&
-        (FMC_SDRAM_DEVICE->SDCMR & FMC_SDCMR_MODE) == 0) {
+    // If FMC clock is already enabled, bootloader did the init
+    if (__HAL_RCC_FMC_IS_CLK_ENABLED()) {
         // SDRAM already initialized, skip re-init
         return;
     }
